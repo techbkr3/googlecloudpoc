fab1615217802fee290b395ee2039fdd81315bae
Mahesh Natarajan <umamags@gmail.com>
2017-07-30 22:29:41 +0530

    added dropzone upload

--- src/main/webapp/js/directives.js
+++ src/main/webapp/js/directives.js
@@ -1,9 +1,49 @@
 'use strict';
-var importBillsDirectives = angular.module('importBillsDirectives', []).config( [
-'$compileProvider',
-function( $compileProvider )
-{
-$compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|file|blob|cdvfile):|data:image\//);
-}
-]);
+var importBillsDirectives = angular.module('importBillsDirectives', []);
+
+importBillsDirectives.directive('dropZone', function () {
+    return {
+        scope: {
+            action: "@"
+        },
+        link: function (scope, element, attrs) {
+            console.log("Creating dropzone");
+
+            // Autoprocess the form
+            if (scope.autoProcess != null && scope.autoProcess == "false") {
+                scope.autoProcess = false;
+            } else {
+                scope.autoProcess = true;
+            }
+
+            // Max file size
+            if (scope.dataMax == null) {
+                scope.dataMax = Dropzone.prototype.defaultOptions.maxFilesize;
+            } else {
+                scope.dataMax = parseInt(scope.dataMax);
+            }
+
+            // Message for the uploading
+            if (scope.message == null) {
+                scope.message = Dropzone.prototype.defaultOptions.dictDefaultMessage;
+            }
+
+            element.dropzone({
+                url: scope.action,
+                maxFilesize: scope.dataMax,
+                paramName: "file",
+                acceptedFiles: scope.mimetypes,
+                maxThumbnailFilesize: scope.dataMax,
+                dictDefaultMessage: scope.message,
+                autoProcessQueue: scope.autoProcess,
+                success: function (file, response) {
+                	console.log(file);
+                    if (scope.callBack != null) {
+                        scope.callBack({response: response});
+                    }
+                }
+            });
+        }
+    }
+});
 
